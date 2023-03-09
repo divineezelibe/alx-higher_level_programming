409731f00405b1a98e22b60aa4e147dcb96ca31a
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-03-09 11:45:12 +0000

    Complete this source code in order to print the integer stored in the variable number, followed by Battery street, followed by a new line.?

---
+++ 0x00-python-hello_world/3-print_number.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))#!/usr/bin/python3
+number = 98
+print("{:d} Battery street".format(number))
