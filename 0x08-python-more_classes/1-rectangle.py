53e7afaf94cde4149a5afd39cb9c764ab87ffec4
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-04-05 14:06:27 +0000

    Updated the error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,7 +4,7 @@
 class Rectangle:
 	"""Rectangle class build up"""
 
-        def __init__(self, width=0, height=0):
+        def __init__(self, width = 0, height = 0):
                 """Start a rectangle"""
 		self.__width = width
         	self.__height = height
@@ -15,21 +15,22 @@ class Rectangle:
                 return self.__width
 
         @width.setter
-        def width(self,value):
+        def width(self, value):
                 if not isinstance(value, int):
                         raise TypeError("width must be an integer")
-                elif value < 0:
+                if value < 0:
                         raise ValueError("width must be >=0")
                 self.__width = value
 
         @property
         def height(self):
+                """etter for height"""
                 return self.__height
 
         @height.setter
         def height (self, value):
                 if not isinstance(value, int):
                         raise TypeError("height must be an integer")
-                elif value < 0:
+                if value < 0:
                         raise ValueError("height must be >= 0")
                 self.__height = value
