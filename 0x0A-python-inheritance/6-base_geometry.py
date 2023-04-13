612c58d67b9b8a559eca06ddce3a6241106cc2b7
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-04-13 02:26:46 +0000

    Created a new module 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A none empty module with BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """A class named BaseGeometry"""
+
+    @classmethod
+    def area(self):
+        """This method calculates the area"""
+        
+        raise Exception("area() is not implemented")
