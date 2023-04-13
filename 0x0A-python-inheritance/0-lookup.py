1f39dfc17620d1c93bb0bcb5c955c9aab795a471
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-04-13 00:55:49 +0000

    made file executable

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,5 +6,5 @@ attributes and methods of an object
 
 
 def lookup(obj):
-    """Funtion here"""
+    """Function that returns the list of attributes and methods of an object"""
     return dir(obj)
