650256206142846e3fd2b18f184f8de004911b1d
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-04-14 02:47:29 +0000

    A module that writes an Object to a text file, using a JSON representation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A module that writes an Object to a 
+text file, using a JSON representation
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """A method to perform this"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
