8c61844b0b6f015dfbf6d75e8bdfd91acf88d2d8
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-04-14 02:21:15 +0000

    Updated file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""I created a module to read a text file"""
+
 
 def read_file(filename="")
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+    """A method to read and print file content to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
