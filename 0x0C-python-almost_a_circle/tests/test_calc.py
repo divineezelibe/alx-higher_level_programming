bfaad0eb3cee241800f8f98d9e5c30b5db2a376d
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-04-19 00:01:08 +0100

    Update test_calc.py

--- 0x0C-python-almost_a_circle/tests/test_calc.py
+++ 0x0C-python-almost_a_circle/tests/test_calc.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 import unittest
-import calculator
+import test_calculator
 
 class TestCalcu(unittest.TestCase):
     
     def test_add(self):
-        cal_ob=calculator.CalculateIt()
+        cal_ob=test_calculator.CalculateIt()
         result=cal_ob.add(2,3)
         self.assertEqual(result,5)
 
