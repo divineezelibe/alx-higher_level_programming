c23df99430a23459aa6272b40130f1e2cbd9b189
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-07-02 03:55:18 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python3
+
 import urllib.request
 
+"""
+Display the body of the response.
+- type: the type of the response content.
+- content: the actual body of the response.
+"""
 url = 'https://alx-intranet.hbtn.io/status'
 
+# Open the URL and retrieve the response
 with urllib.request.urlopen(url) as response:
+    # Read the response content and decode it as UTF-8
     body = response.read().decode('utf-8')
-
+    
 print("Body response:")
 print("\t- type:", type(body))
 print("\t- content:", body)
