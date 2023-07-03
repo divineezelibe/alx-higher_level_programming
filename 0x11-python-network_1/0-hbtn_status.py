38a133b48bb72532ac9292376dfe51431b3454fa
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-07-03 14:16:00 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,13 @@
 #!/usr/bin/python3
-"""
-Display the body of the response.
-- type: the type of the response content.
-- content: the actual body of the response.
-"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
 
-import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utfContent = body.decode("utf-8")
-    
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", utfContent)
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
